2e0c61ab8a962404665eff6c84424d9b6007855a
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2026-02-02 15:29:21 +0000

    about: Remove link
    Doesn't work in root apps unless we jump through hoops to identify
    the request user and drop privileges.
    
    We don't show a link in other root apps.
    
    Fixes https://github.com/linuxmint/mintsysadm/issues/5

--- usr/lib/linuxmint/mintsysadm/mintsysadm.py
+++ usr/lib/linuxmint/mintsysadm/mintsysadm.py
@@ -259,7 +259,6 @@ GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT {boot_args}"
         dlg.set_version("__DEB_VERSION__")
         dlg.set_icon_name("mintsysadm")
         dlg.set_logo_icon_name("mintsysadm")
-        dlg.set_website("https://www.github.com/linuxmint/mintsysadm")
         def close(w, res):
             if res == Gtk.ResponseType.CANCEL or res == Gtk.ResponseType.DELETE_EVENT:
                 w.destroy()
